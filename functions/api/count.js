9fbfe2f94f8e6180c4c82a93d0c710a2c91ab7bf
AlexLin625 <me@a1exlin.cn>
2025-03-14 17:12:30 +0800

    迁移到云数据库

--- functions/api/count.js
+++ functions/api/count.js
@@ -1,24 +1,31 @@
 export async function onRequest({ request }) {
-    const targetUrl = 'https://api.a1exlin.cn/api/count';
+  const targetUrl =
+    "https://shuiyu-sql-proxy.linhongjie625.workers.dev/count";
 
-    const init = {
-        method: request.method,
-        headers: request.headers,
-        body: request.method !== 'GET' && request.method !== 'HEAD' ? request.body : null,
-    };
+  const init = {
+    method: request.method,
+    headers: request.headers,
+    body:
+      request.method !== "GET" && request.method !== "HEAD"
+        ? request.body
+        : null,
+  };
 
-    const response = await fetch(targetUrl, init);
+  const response = await fetch(targetUrl, init);
 
-    const newHeaders = new Headers(response.headers);
-    newHeaders.set('Access-Control-Allow-Origin', '*');
-    newHeaders.set('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
-    newHeaders.set('Access-Control-Allow-Headers', '*');
+  const newHeaders = new Headers(response.headers);
+  newHeaders.set("Access-Control-Allow-Origin", "*");
+  newHeaders.set(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, DELETE"
+  );
+  newHeaders.set("Access-Control-Allow-Headers", "*");
 
-    const body = await response.body;
+  const body = await response.body;
 
-    return new Response(body, {
-        status: response.status,
-        statusText: response.statusText,
-        headers: newHeaders
-    });
+  return new Response(body, {
+    status: response.status,
+    statusText: response.statusText,
+    headers: newHeaders,
+  });
 }
